2c5e8fe7fb28c9b3b0da7a62a5c3954e8021cf68
Joergen Ibsen <ji@ibse.dk>
2016-06-17 23:32:42 +0200

    Implement jump table as switch for non-GCC
    This overlays the jump table implementation, which uses a GCC
    extension, with an equivalent switch statement. Preprocessor
    checks switch between the two.

--- src/lzvn_decode_base.c
+++ src/lzvn_decode_base.c
@@ -23,6 +23,14 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSI
 
 #include "lzvn_decode_base.h"
 
+#if !defined(HAVE_LABELS_AS_VALUES)
+#  if defined(__GNUC__) || defined(__clang__)
+#    define HAVE_LABELS_AS_VALUES 1
+#  else
+#    define HAVE_LABELS_AS_VALUES 0
+#  endif
+#endif
+
 //  Both the source and destination buffers are represented by a pointer and
 //  a length; they are *always* updated in concert using this macro; however
 //  many bytes the pointer is advanced, the length is decremented by the same
@@ -37,6 +45,7 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSI
   (state->src = src_ptr, state->dst = dst_ptr, state->d_prev = D)
 
 void lzvn_decode(lzvn_decoder_state *state) {
+#if HAVE_LABELS_AS_VALUES
   // Jump table for all instructions
   static const void *opc_tbl[256] = {
       &&sml_d, &&sml_d, &&sml_d, &&sml_d, &&sml_d, &&sml_d, &&eos,   &&lrg_d,
@@ -71,7 +80,7 @@ void lzvn_decode(lzvn_decoder_state *state) {
       &&sml_l, &&sml_l, &&sml_l, &&sml_l, &&sml_l, &&sml_l, &&sml_l, &&sml_l,
       &&lrg_m, &&sml_m, &&sml_m, &&sml_m, &&sml_m, &&sml_m, &&sml_m, &&sml_m,
       &&sml_m, &&sml_m, &&sml_m, &&sml_m, &&sml_m, &&sml_m, &&sml_m, &&sml_m};
-
+#endif
   size_t src_len = state->src_end - state->src;
   size_t dst_len = state->dst_end - state->dst;
   if (src_len == 0 || dst_len == 0)
@@ -99,8 +108,13 @@ void lzvn_decode(lzvn_decoder_state *state) {
   }
 
   unsigned char opc = src_ptr[0];
-  goto *opc_tbl[opc];
 
+#if HAVE_LABELS_AS_VALUES
+  goto *opc_tbl[opc];
+#else
+  for (;;) {
+    switch (opc) {
+#endif
 //  ===============================================================
 //  These four opcodes (sml_d, med_d, lrg_d, and pre_d) encode both a
 //  literal and a match. The bulk of their implementations are shared;
@@ -113,6 +127,128 @@ void lzvn_decode(lzvn_decoder_state *state) {
 //  the source has enough length to represent the full opcode before
 //  reading past the first byte.
 sml_d:
+#if !HAVE_LABELS_AS_VALUES
+  case 0:
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 8:
+  case 9:
+  case 10:
+  case 11:
+  case 12:
+  case 13:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+  case 20:
+  case 21:
+  case 24:
+  case 25:
+  case 26:
+  case 27:
+  case 28:
+  case 29:
+  case 32:
+  case 33:
+  case 34:
+  case 35:
+  case 36:
+  case 37:
+  case 40:
+  case 41:
+  case 42:
+  case 43:
+  case 44:
+  case 45:
+  case 48:
+  case 49:
+  case 50:
+  case 51:
+  case 52:
+  case 53:
+  case 56:
+  case 57:
+  case 58:
+  case 59:
+  case 60:
+  case 61:
+  case 64:
+  case 65:
+  case 66:
+  case 67:
+  case 68:
+  case 69:
+  case 72:
+  case 73:
+  case 74:
+  case 75:
+  case 76:
+  case 77:
+  case 80:
+  case 81:
+  case 82:
+  case 83:
+  case 84:
+  case 85:
+  case 88:
+  case 89:
+  case 90:
+  case 91:
+  case 92:
+  case 93:
+  case 96:
+  case 97:
+  case 98:
+  case 99:
+  case 100:
+  case 101:
+  case 104:
+  case 105:
+  case 106:
+  case 107:
+  case 108:
+  case 109:
+  case 128:
+  case 129:
+  case 130:
+  case 131:
+  case 132:
+  case 133:
+  case 136:
+  case 137:
+  case 138:
+  case 139:
+  case 140:
+  case 141:
+  case 144:
+  case 145:
+  case 146:
+  case 147:
+  case 148:
+  case 149:
+  case 152:
+  case 153:
+  case 154:
+  case 155:
+  case 156:
+  case 157:
+  case 192:
+  case 193:
+  case 194:
+  case 195:
+  case 196:
+  case 197:
+  case 200:
+  case 201:
+  case 202:
+  case 203:
+  case 204:
+  case 205:
+#endif
   UPDATE_GOOD;
   // "small distance": This opcode has the structure LLMMMDDD DDDDDDDD LITERAL
   //  where the length of literal (0-3 bytes) is encoded by the high 2 bits of
@@ -134,6 +270,40 @@ sml_d:
   goto copy_literal_and_match;
 
 med_d:
+#if !HAVE_LABELS_AS_VALUES
+  case 160:
+  case 161:
+  case 162:
+  case 163:
+  case 164:
+  case 165:
+  case 166:
+  case 167:
+  case 168:
+  case 169:
+  case 170:
+  case 171:
+  case 172:
+  case 173:
+  case 174:
+  case 175:
+  case 176:
+  case 177:
+  case 178:
+  case 179:
+  case 180:
+  case 181:
+  case 182:
+  case 183:
+  case 184:
+  case 185:
+  case 186:
+  case 187:
+  case 188:
+  case 189:
+  case 190:
+  case 191:
+#endif
   UPDATE_GOOD;
   //  "medium distance": This is a minor variant of the "small distance"
   //  encoding, where we will now use two extra bytes instead of one to encode
@@ -150,6 +320,28 @@ med_d:
   goto copy_literal_and_match;
 
 lrg_d:
+#if !HAVE_LABELS_AS_VALUES
+  case 7:
+  case 15:
+  case 23:
+  case 31:
+  case 39:
+  case 47:
+  case 55:
+  case 63:
+  case 71:
+  case 79:
+  case 87:
+  case 95:
+  case 103:
+  case 111:
+  case 135:
+  case 143:
+  case 151:
+  case 159:
+  case 199:
+  case 207:
+#endif
   UPDATE_GOOD;
   //  "large distance": This is another variant of the "small distance"
   //  encoding, where we will now use two extra bytes to encode the match
@@ -164,6 +356,20 @@ lrg_d:
   goto copy_literal_and_match;
 
 pre_d:
+#if !HAVE_LABELS_AS_VALUES
+  case 70:
+  case 78:
+  case 86:
+  case 94:
+  case 102:
+  case 110:
+  case 134:
+  case 142:
+  case 150:
+  case 158:
+  case 198:
+  case 206:
+#endif
   UPDATE_GOOD;
   //  "previous distance": This opcode has the structure LLMMM110, where the
   //  length of the literal (0-3 bytes) is encoded by the high 2 bits of the
@@ -269,7 +475,11 @@ copy_match:
   //  the appropriate implementation.
   PTR_LEN_INC(dst_ptr, dst_len, M);
   opc = src_ptr[0];
+#if HAVE_LABELS_AS_VALUES
   goto *opc_tbl[opc];
+#else
+  break;
+#endif
 
 // ===============================================================
 // Opcodes representing only a match (no literal).
@@ -279,6 +489,23 @@ copy_match:
 //  sequence from the literal and match opcodes to perform the actual
 //  copy implementation.
 sml_m:
+#if !HAVE_LABELS_AS_VALUES
+  case 241:
+  case 242:
+  case 243:
+  case 244:
+  case 245:
+  case 246:
+  case 247:
+  case 248:
+  case 249:
+  case 250:
+  case 251:
+  case 252:
+  case 253:
+  case 254:
+  case 255:
+#endif
   UPDATE_GOOD;
   //  "small match": This opcode has no literal, and uses the previous match
   //  distance (i.e. it encodes only the match length), in a single byte as
@@ -291,6 +518,9 @@ sml_m:
   goto copy_match;
 
 lrg_m:
+#if !HAVE_LABELS_AS_VALUES
+  case 240:
+#endif
   UPDATE_GOOD;
   //  "large match": This opcode has no literal, and uses the previous match
   //  distance (i.e. it encodes only the match length). It is encoded in two
@@ -310,6 +540,23 @@ lrg_m:
 //  match length or match distance to worry about (but we need to *not*
 //  touch D, as it must be preserved between opcodes).
 sml_l:
+#if !HAVE_LABELS_AS_VALUES
+  case 225:
+  case 226:
+  case 227:
+  case 228:
+  case 229:
+  case 230:
+  case 231:
+  case 232:
+  case 233:
+  case 234:
+  case 235:
+  case 236:
+  case 237:
+  case 238:
+  case 239:
+#endif
   UPDATE_GOOD;
   //  "small literal": This opcode has no match, and encodes only a literal
   //  of length up to 15 bytes. The format is 1110LLLL LITERAL.
@@ -318,6 +565,9 @@ sml_l:
   goto copy_literal;
 
 lrg_l:
+#if !HAVE_LABELS_AS_VALUES
+  case 224:
+#endif
   UPDATE_GOOD;
   //  "large literal": This opcode has no match, and uses the previous match
   //  distance (i.e. it encodes only the match length). It is encoded in two
@@ -371,20 +621,35 @@ copy_literal:
   PTR_LEN_INC(src_ptr, src_len, L);
   //  Load the first byte of the next opcode, and jump to its implementation.
   opc = src_ptr[0];
+#if HAVE_LABELS_AS_VALUES
   goto *opc_tbl[opc];
+#else
+  break;
+#endif
 
 // ===============================================================
 // Other opcodes
 nop:
+#if !HAVE_LABELS_AS_VALUES
+  case 6:
+  case 14:
+#endif
   UPDATE_GOOD;
   opc_len = 1;
   if (src_len <= opc_len)
     return; // source truncated
   PTR_LEN_INC(src_ptr, src_len, opc_len);
   opc = src_ptr[0];
+#if HAVE_LABELS_AS_VALUES
   goto *opc_tbl[opc];
+#else
+  break;
+#endif
 
 eos:
+#if !HAVE_LABELS_AS_VALUES
+  case 22:
+#endif
   opc_len = 8;
   if (src_len < opc_len)
     return; // source truncated (here we don't need an extra byte for next op
@@ -397,7 +662,50 @@ eos:
 // ===============================================================
 // Return on error
 udef:
+#if !HAVE_LABELS_AS_VALUES
+  case 30:
+  case 38:
+  case 46:
+  case 54:
+  case 62:
+  case 112:
+  case 113:
+  case 114:
+  case 115:
+  case 116:
+  case 117:
+  case 118:
+  case 119:
+  case 120:
+  case 121:
+  case 122:
+  case 123:
+  case 124:
+  case 125:
+  case 126:
+  case 127:
+  case 208:
+  case 209:
+  case 210:
+  case 211:
+  case 212:
+  case 213:
+  case 214:
+  case 215:
+  case 216:
+  case 217:
+  case 218:
+  case 219:
+  case 220:
+  case 221:
+  case 222:
+  case 223:
+#endif
 invalid_match_distance:
 
   return; // we already updated state
+#if !HAVE_LABELS_AS_VALUES
+    }
+  }
+#endif
 }
